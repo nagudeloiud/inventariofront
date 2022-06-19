9c152bbf68e15eb0c9ecb4a9c31640191491ff4d
nagudeloiud <noberto.agudelo@est.iudigital.edu.co>
2022-06-18 20:49:01 -0500

    :sparkles: refactoring code with components Modal and  TablaModulos and changes to Estado

--- src/components/estado/Estado.js
+++ src/components/estado/Estado.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState} from 'react'
 import { obtenerTodos, guardar } from '../../services/EstadoService';
+import TablaModulos from '../iu/TablaModulos';
+import Modal from './Modal';
 
 export default function Estado() {
 
-  const [estados, setEstados] = useState([]);   
-
+  const [estados, setEstados] = useState([]);  
   const [estado, setEstado] = useState({
+    _id: '',
     nombre: '',
-    estado: false
+    estado: true
   });   
   const [error, setError] = useState(false);
-
+  const [hidden] = useState('hidden');
+  const [loading, setLoading] = useState(false);
 
 
 
@@ -38,9 +41,15 @@ export default function Estado() {
   
 
   const add = e => {
+    setLoading(true);
     e.preventDefault();
-    console.log(estado)
-    guardarEstado();
+    console.log(estado);
+    if(estado._id){
+      editarEstado();
+    }else{
+      guardarEstado();
+    }
+    resetEstado();
   }; 
 
   const guardarEstado = () => { 
@@ -48,9 +57,11 @@ export default function Estado() {
     .then( r =>{
       setEstados([...estados, r.data])
       changeError(false);  
+      setLoading(false);
     }).catch(e =>{
       console.log(e);
-      changeError(true);  
+      changeError(true); 
+      setLoading(false); 
     }) 
   };
 
@@ -64,6 +75,38 @@ export default function Estado() {
     changeError(false)
   }
 
+  const openEditById = e => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      const id = e.target.getAttribute('data');
+      console.log(id)
+      const estadoFilter = estados.filter(est => est._id == id)[0];
+      setEstado({
+        ...estadoFilter
+      });
+    }, 500)
+  }
+
+  const editarEstado = () => {
+    editarPorId(estado._id, estado)
+    .then(r => {
+      console.log(r.data._id)
+      const id = r.data._id;
+      if(!r.data.estado){
+        const activos = estados.filter(est => est._id !== id);
+        setEstados(activos);
+      }
+      changeError(false)
+      setLoading(false);
+    }).catch(e => {
+      console.log(e);
+      changeError(true);
+      setLoading(false);
+    })
+  }
+
   const resetEstado = () => {
     setEstado({
       _id: '',
@@ -72,140 +115,34 @@ export default function Estado() {
     })
   }
 
-  
-
-
+ 
 
 
   return (
     <div className="container">      
-      <div className='table-responsive'>
-        <button 
-          type="button" 
-          className="btn btn-outline-primary"
-          data-bs-toggle="modal" 
-          data-bs-target="#exampleModal"          
-          >
-          <i className="fa-solid fa-plus"></i>
-            Agregar            
-         </button>
+      <button 
+        type="button" 
+        className="btn btn-outline-primary"
+        data-bs-toggle="modal" 
+        data-bs-target="#exampleModal"          
+        >
+        <i className="fa-solid fa-plus"></i>
+          Agregar            
+      </button>
 
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Estado</th>
-              <th scope="col">Fecha Creación</th>
-              <th scope="col">Fecha Actualización</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              estados.map((est, index) => {
-                const date = new Date(est.fechaCreacion);
-                const creacion = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay();
-                return(
-                  <tr key={est._id}>
-                    <th scope="row">{ index + 1}</th>
-                    <td>{est.nombre}</td>
-                    <td>{est.estado ? 'Activo' : 'Inactivo'}</td>
-                    <td>{creacion}</td>
-                    <td>{est.fechaActualizacion}</td>
-                    <td>
-                      <button 
-                        type="button" 
-                        className="btn btn-outline-success"
-                        data-bs-toggle="modal" 
-                        data-bs-target="#exampleModal"     
-                        data = {est._id}
-                        
-
-                       >                                            
-                        <i className="fa-solid fa-pen-to-square"></i>
-                        -
-                      </button>
-                      <button 
-                        type="button" 
-                        className="btn btn-outline-danger">             
-                        <i className="fa-solid fa-trash"></i>
-                        -
-                      </button>
-                    </td>    
-                    <td></td>
-                  </tr>  
-                )
-              })
-            }
-          </tbody>
-        </table>
-      </div>     
-
-
-
-      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Nuevo Estado</h5>
-              <button 
-                type="button" 
-                className="btn-close" 
-                data-bs-dismiss="modal" 
-                aria-label="Close" 
-                onClick={closeModal}>
-              </button>
-            </div>
-            <div className="modal-body">
-              <form onSubmit={add}>
-                <div className="mb-3">
-                  <label htmlFor="recipient-name" className="col-form-label">Nombre:</label>
-                  <input 
-                    required 
-                    value={estado.nombre} 
-                    name = "nombre" 
-                    type="text" 
-                    className="form-control" 
-                    onChange={changeEstado}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="message-text" className="col-form-label">Estado:</label>
-                  <select 
-                    required 
-                    className="form-select" 
-                    aria-label="Default select example" 
-                    value={estado.estado} 
-                    name="estado" 
-                    onChange={changeEstado}>
-                    <option value={true}>Activo</option>
-                    <option value={false}>Inactivo</option>                    
-                  </select>                  
-                                  </div>
-                <div className="modal-footer">                   
-                  <div className={error ? 'alert alert-danger': 'd-none'} role="alert">
-                    ¡Ha ocurrido un error al guardar!
-                  </div>
-                  <button 
-                    type="button" 
-                    className="btn btn-secondary" 
-                    data-bs-dismiss="modal"
-                    onClick={closeModal}
-                  >
-                    Close
-                  </button>
-                  <button type="submit" className="btn btn-primary">Guardar</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-
+      <TablaModulos 
+        items={estados}
+        openEditById={openEditById}
+      />
+      <Modal 
+        estado={estado}
+        loading={loading}
+        closeModal={closeModal}
+        hidden={hidden}
+        changeEstado={changeEstado}
+        error={error}
+        add={add}
+      /> 
 
     </div>
   )
---
+++ src/components/iu/TablaModulos.js
@@ -0,0 +1,64 @@
+/** Componente llamado TablaModulos que sirve para todos los modulos que manejen los mismos datos(campos-atributos):
+ *  _id, nombre, estado, fechaCreacion, fechaActualizacion.
+ *   A saber serviría para estados, marcas, tiposequipo
+ * 
+ */
+
+import React from 'react'
+
+export default function TablaModulos({items, openEditById}) {
+  return (
+    <div className="table-responsive">
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Estado</th>
+          <th scope="col">Fecha Creación</th>
+          <th scope="col">Fecha Actualización</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          items.map((item, index) => {
+            const date = new Date(item.fechaCreacion);
+            const creacion = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDay();
+            return(
+              <tr key={item._id}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.nombre}</td>
+                <td>{item.estado ? 'Activo' : 'Inactivo'}</td>
+                <td>{creacion}</td>
+                <td>{item.fechaActualizacion}</td>
+                <td>
+                  <button 
+                    type="button" 
+                    className="btn btn-outline-success"
+                    data-bs-toggle="modal" 
+                    data-bs-target="#exampleModal"
+                    data={item._id}
+                    onClick={openEditById}
+                  >
+                    <i className="fa-solid fa-pen-to-square"></i>
+                    -
+                  </button>
+                  <button 
+                    type="button" 
+                    className="btn btn-outline-danger"
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                    -
+                  </button>
+                </td>
+                <td></td>
+            </tr>
+            );
+          })
+        }
+      </tbody>
+    </table>
+  </div>
+  )
+}
